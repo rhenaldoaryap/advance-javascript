1b035a77e399137256902a407ff6a0092cc3b9f3
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-13 17:07:13 +0700

    learn about method inside of a class and how to call it

--- objects.js
+++ objects.js
@@ -6,6 +6,8 @@ const job = {
 };
 
 console.log(job.title);
+// Output:
+// Developer
 
 // Create an Object with "new" keyword and Date() built-in at JavaScript
 console.log(new Date().toISOString());
@@ -20,4 +22,32 @@ class Job {
 }
 
 const developer = new Job("Developer", "New York", 50000);
-const cook = new Job("Cook", "Cibitung", 35000);
+const cook = new Job("Chef", "Cibitung", 35000);
+console.log(developer, cook);
+// Output:
+// Job { title: "Developer", location: "New York", salary: 50000 }
+// Job { title: "Chef", location: "Cibitung", salary: 35000 }
+
+// Using a method inside of a class
+class Job2 {
+  constructor(jobTitle, location, salary) {
+    this.title = jobTitle;
+    this.place = location;
+    this.salary = salary;
+  }
+
+  describe() {
+    console.log(
+      `I'm a ${this.title}, I work in ${this.place}, I earn ${this.salary} dollar per year`
+    );
+  }
+}
+
+const developer2 = new Job2("Developer", "Jakarta", 45000);
+const cook2 = new Job2("Chef", "Cibitung", 35000);
+
+developer2.describe();
+cook2.describe();
+// Output:
+// I'm a Developer, I work in New York, I earn 45000 dollar per year
+// I'm a Chef, I work in Cibitung, I earn 35000 dollar per year
