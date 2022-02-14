62498acc7a1f3555ba910b980b07c63d825b4bce
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-14 16:55:49 +0700

    learn about asynchronous code and callback function

---
+++ async.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+// Without using asynchronous code and callback function
+function readFile() {
+  let fileData;
+
+  fileData = fs.readFileSync("data.txt");
+
+  console.log(fileData.toString());
+  console.log("Hi There!");
+  console.log("-- Below here using asynchronous code and callback function --");
+}
+
+readFile();
+
+// With using asynchronous code and callback function
+function anotherReadFile() {
+  let fileData;
+
+  fs.readFile("data.txt", function (error, fileData) {
+    console.log("File parsing done!");
+    console.log(fileData.toString());
+  });
+
+  console.log("Hi There!");
+}
+
+anotherReadFile();
