68aa295728b6a2d173abdb181e53a570df402fad
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-15 13:15:11 +0700

    learning how to use promise in JavaScript

--- async.js
+++ async.js
@@ -1,13 +1,16 @@
-const fs = require("fs");
+const fileSystem = require("fs");
+const fs = require("fs").promises;
 
 // Without using asynchronous code and callback function
 function readFile() {
   let fileData;
 
-  fileData = fs.readFileSync("data.txt");
+  fileData = fileSystem.readFileSync("data.txt");
 
   console.log(fileData.toString());
-  console.log("Hi There!");
+  console.log(
+    "Hi There! Here without using asynchronous code and callback function"
+  );
   console.log("-- Below here using asynchronous code and callback function --");
 }
 
@@ -18,12 +21,27 @@ function anotherReadFile() {
   let fileData;
   // this container can be omitted
 
-  fs.readFile("data.txt", function (error, fileData) {
-    console.log("File parsing done!");
+  fileSystem.readFile("data.txt", function (error, fileData) {
+    console.log("File parsing done without using promise!");
+    console.log(fileData.toString());
+  });
+
+  console.log("Hi There! Here using asynchronous code");
+  console.log("-- Below here using Promises for avoiding Callback Hell");
+}
+
+anotherReadFile();
+
+// Using Promises
+function readFileAgain() {
+  let fileData;
+
+  fs.readFile("data.txt").then(function (fileData) {
+    console.log("Parsing file done! using Promise");
     console.log(fileData.toString());
   });
 
   console.log("Hi There!");
 }
 
-anotherReadFile();
+readFileAgain();
