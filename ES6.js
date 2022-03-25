f196f59bf287c0c49d1b21991f883199c0d2335a
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-25 13:42:03 +0700

    learn about synctatic sugar, concise method ES6, relearn .then().catch()

--- ES6.js
+++ ES6.js
@@ -123,3 +123,79 @@ const removeFirstTwoArray = (list) => {
 
 const arr = removeFirstTwoArray(source);
 console.log(...arr); // 3 4 5 6 7 8 9 10 (because we using spread operator to taking out all values from objects/array)
+
+/*
+  Writing object literals using object property shorthand
+*/
+
+/*
+  Before using shorthand for creating object literals
+
+const createPerson = (name, age, gender) => {
+  return {
+    name: name,
+    age: age,
+    gender: gender,
+  };
+};
+
+console.log(createPerson("Rhenaldo", 23, "Male"));
+*/
+
+/*
+  With shorthand for avoiding the redudant (or called syntatic sugar)
+*/
+
+const createPerson = (name, age, gender) => {
+  return { name, age, gender };
+};
+
+console.log(createPerson("Rhenaldo", 23, "Male"));
+
+/*
+  Write concise (ringkas) declarative functions with ES6
+  Before that, we already known to create a method inside an object with ES5 like code below:
+*/
+
+const person = {
+  name: "Rhenaldo",
+  sayHello: function () {
+    return `Hello ${this.name}`;
+  },
+};
+
+console.log(person.sayHello());
+
+/*
+  create a method with ES6
+*/
+
+const personAgain = {
+  name: "Arya",
+  sayHello() {
+    return `Hello, ${this.name}`;
+  },
+};
+
+console.log(personAgain.sayHello());
+
+/*
+  .then()
+  .catch()
+  method only available if we are not inside of a function
+  if we do so, we have to use async-await to handle the asynchronous
+*/
+const makeServerRequest = new Promise((resolve, reject) => {
+  // let serverRequest = true;
+  let serverRequest = false;
+
+  serverRequest ? resolve("We got the data") : reject("data not received");
+});
+
+makeServerRequest
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
