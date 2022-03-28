0009a12e3feae28bee2d7a2abac4e4c9893fb6a3
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-28 17:05:19 +0700

    learn about spread operator, nested array and object, iterate through the keys of an object

--- DSA.js
+++ DSA.js
@@ -88,3 +88,165 @@ console.log(
     result:
     ["warm", "sunny"]
 */
+
+/*
+    The simplest way to copying a value of an array with using Spead Operator.
+
+    You already know how to use Spread Operator.
+    Basically, Spread Operator will taking out the value or copying the value want to copy.
+*/
+
+const copyMachine = (arr, num) => {
+  let newArr = [];
+  while (num >= 1) {
+    newArr.push([...arr]);
+    num--;
+  }
+  return newArr;
+};
+
+console.log(copyMachine(["true", "false", "true"], 2));
+
+/*
+    result:
+    [ [ 'true', 'false', 'true' ], [ 'true', 'false', 'true' ] ]
+*/
+
+const spreadOut = () => {
+  let fragment = ["to", "code"];
+  let sentence = ["learning", ...fragment, "is", "fun"];
+  return sentence;
+};
+
+console.log(spreadOut());
+
+/*
+    result:
+    ["learning", "to", "code", "is", "fun"]
+*/
+
+let nestedArray = [
+  ["deep"],
+  [["deeper"], ["deeper"]],
+  [[["deepest"], ["deepest"]], [[["deepest-est?"]]]],
+];
+
+console.log(nestedArray[2][1][0][0][0]);
+
+/*
+    Accessing value inside of nested object.
+
+    If we accessing nested an array with square bracket and count it what number of array
+    that contain value we looking for, for looking value inside of nested object we using
+    the . (dot) notation.
+*/
+
+let nestedObject = {
+  id: 28802695164,
+  date: "December 31, 2016",
+  data: {
+    totalUsers: 99,
+    online: 80,
+    onlineStatus: {
+      active: 67,
+      away: 13,
+      busy: 8,
+    },
+  },
+};
+
+console.log((nestedObject.data.onlineStatus.busy = 10)); // 10
+
+/*
+    Access property name inside of Object using square bracket.
+
+    the way we accessing value inside of an Object using square bracket can be very helpful.
+    this way only available if we inside of a function.
+*/
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
+};
+
+const checkInventory = (scannedItems) => {
+  return foods[scannedItems];
+};
+
+console.log(checkInventory("oranges")); // 32
+
+/*
+    Check if an object has property.
+
+    JavaScript provide us two different ways to do this.
+    first, use the .hasOwnProperty() method
+    second, use in, for instance "Aldo" in users;
+*/
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true,
+  },
+  Jeff: {
+    age: 32,
+    online: true,
+  },
+  Sarah: {
+    age: 48,
+    online: true,
+  },
+  Ryan: {
+    age: 19,
+    online: true,
+  },
+};
+
+const isEveryoneHere = (userObj) => {
+  return ["Alan", "Jeff", "Sarah", "Ryan"].every((name) =>
+    userObj.hasOwnProperty(name)
+  );
+};
+
+console.log(isEveryoneHere(users)); // true
+
+/*
+    Explanation:
+    every method will determines whether all the members of an array satisfy the specified test.
+    and NOT RETURN A NEW ARRAY.
+*/
+
+/*
+    Iterate through the keys of an object with a for .. in statement.
+
+    Sometimes you may need to iterate through all the keys within an object.
+    This requires a specific syntax in JavaScript called a for...in statement.
+*/
+
+const usersAgain = {
+  Alan: {
+    online: false,
+  },
+  Jeff: {
+    online: true,
+  },
+  Sarah: {
+    online: false,
+  },
+};
+
+const checkOnline = (userObj) => {
+  let result = 0;
+  for (const user in userObj) {
+    if (userObj[user].online === true) {
+      result++;
+    }
+  }
+  return result;
+};
+
+console.log(checkOnline(usersAgain)); // 1
