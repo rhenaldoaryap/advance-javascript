ef459ef520be0c4c98a0373f3f0596d480f0c92d
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-04-09 16:40:32 +0700

    dive deeper into object destructuring, bind and this keyword

--- objects/assets/scripts/objects.js
+++ objects/assets/scripts/objects.js
@@ -4,7 +4,8 @@ const searchBtn = document.getElementById("search-btn");
 const movies = [];
 
 // updating UI
-const renderMovies = () => {
+// using default values from searching movies
+const renderMovies = (filter = "") => {
   const movieList = document.getElementById("movie-list");
 
   if (movies.length === 0) {
@@ -17,14 +18,56 @@ const renderMovies = () => {
   // cleareance the movie list
   movieList.innerHTML = "";
 
-  // outputting all movies to and individual item with loop
-  movies.forEach((movie) => {
+  /*
+    only render the movie was search
+    if the filter was falsy which mean we don't do search anything return all movies
+    but if we do the search, filter the movies based on the filter was entered by user
+  */
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
+
+  /*
+    outputting all movies to and individual item with loop
+    movies.forEach((movie) => {
+    changed plan, we do loop for movie that already filter (search) by user
+  */
+  filteredMovies.forEach((movie) => {
     const movieEl = document.createElement("li");
-    // get this value from addMovieHanlder function at at newMovie object
+    // using object destructuring
+    const { info, ...otherProps } = movie;
+    console.log(otherProps);
+    /*
+    get this value from addMovieHanlder function at at newMovie object
     let text = movie.info.title + " - ";
-    for (const key in movie.info) {
+    change applied to the object destructuring
+    info below referring to the info property inside of newMovie object
+    and inside of addMovieHandler
+    if you want to use another key when using destructuring object,
+    you could do that by using semilocon and type new name you want,
+    for example:
+    const { title: titleOfFilm } = info
+
+    using bind()
+    this keyword can be very confusing and cumbersome, we can ensure that this keyword
+    referring to the same object or same function we looking to.
+
+    this code below before using bind()
+    const { title: movieTitle } = info;
+    let text = movieTitle + " - ";
+
+    after using bind see the example below.
+    why we referring to movie?
+    because movie is a new array that automatically created by JavaScript for us
+    and we can use that as a pointer / referring to.
+    and then we execute the getFormattedTitle method after we bind it.
+    */
+    let { getFormattedTitle } = movie;
+    getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle() + " - ";
+    for (const key in info) {
       if (key !== "title") {
-        text += `${key}: ${movie.info[key]}`;
+        text += `${key}: ${info[key]}`;
       }
     }
     movieEl.textContent = text;
@@ -55,10 +98,19 @@ const addMovieHandler = () => {
       [extraName]: extraValue,
     },
     id: Math.random().toString(),
+    getFormattedTitle() {
+      return this.info.title.toUpperCase();
+    },
   };
 
   movies.unshift(newMovie);
   renderMovies();
 };
 
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById("filter-title").value;
+  renderMovies(filterTerm);
+};
+
 addMovieBtn.addEventListener("click", addMovieHandler);
+searchBtn.addEventListener("click", searchMovieHandler);
