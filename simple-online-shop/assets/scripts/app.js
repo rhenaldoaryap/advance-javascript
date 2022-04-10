e5dd37cc1b0cbbf41942a30a674370e314988415
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-04-10 21:44:24 +0700

    getting headache with setter and getter and chaining classes

--- simple-online-shop/assets/scripts/app.js
+++ simple-online-shop/assets/scripts/app.js
@@ -19,6 +19,27 @@ class Product {
 class ShoppingCart {
   items = [];
 
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `
+      <h2>Total: \$${this.totalAmount.toFixed(2)}</h2>
+    `;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+    return sum;
+  }
+
+  addProduct(product) {
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
+  }
+
   render() {
     const cartEl = document.createElement("section");
     cartEl.innerHTML = `
@@ -26,6 +47,8 @@ class ShoppingCart {
         <button>Order Now</button>
     `;
     cartEl.className = "cart";
+    // create a new tag element with this keyword
+    this.totalOutput = cartEl.querySelector("h2");
     return cartEl;
   }
 }
@@ -38,8 +61,7 @@ class ProductItem {
   }
 
   addToCart() {
-    console.log("Adding produt success");
-    console.log(this.product);
+    App.addProductToCart(this.product);
   }
 
   render() {
@@ -100,8 +122,8 @@ class Shop {
   render() {
     const renderHook = document.getElementById("app");
 
-    const cart = new ShoppingCart();
-    const cartEl = cart.render();
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
 
     const productList = new ProductList();
     const productListEl = productList.render();
@@ -111,5 +133,16 @@ class Shop {
   }
 }
 
-const shop = new Shop();
-shop.render();
+class App {
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
