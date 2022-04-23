e2a0b5bd0ebb11768773aa07083893ad9ce5ff16
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-04-23 13:44:58 +0700

    optimizing startup time and code usage

---
+++ performance/src/optimized/shop.js
@@ -0,0 +1,24 @@
+import { products } from "./product";
+import { renderProducts } from "./rendering";
+
+function addProduct(event) {
+  import("./product-management.js").then((module) => {
+    module.addProduct();
+  });
+}
+
+function deleteProduct(productId) {
+  import("./product-management").then((module) => {
+    module.deleteProduct(productId);
+  });
+}
+
+function initProducts() {
+  renderProducts(products, deleteProduct);
+}
+
+const addProductForm = document.getElementById("new-product");
+
+initProducts();
+
+addProductForm.addEventListener("submit", addProduct);
